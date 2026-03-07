26691453e055af0773551833c1ae90d581e98fd6
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-03-07 11:12:52 +0530

    added changes

---
+++ 03-Loops/PrintHelloWorld.java
@@ -0,0 +1,14 @@
+//Printing helloworld multiple times using loops to understand working of loops//
+public class PrintHelloWorld {
+    public static void main(String[] args) {        
+        for(int counter=0; counter<3; counter= counter+1){
+            System.out.println("Helloworld");
+
+        }
+
+        
+      
+    }
+    
+    
+}
