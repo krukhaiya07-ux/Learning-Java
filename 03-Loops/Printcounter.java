b1367f33aa6bb4c0a1941bed2e716e7780101493
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-03-07 11:27:03 +0530

    added changes

---
+++ 03-Loops/Printcounter.java
@@ -0,0 +1,9 @@
+
+public class Printcounter {
+    public static void main(String[] args) {
+        for(int counter=1; counter<11; counter=counter+1){
+        System.out.println(counter);
+        }
+    }
+    
+}
