a978472bf0457be173fbed284392e02a6e1e7bc8
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-03-08 09:13:20 +0530

    initial commit

---
+++ 04-Arrays/SimpleProgram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class SimpleProgram{
+    public static void main(String[] args) {
+        int[]marks=new int[3];
+             marks[0]=98;//physics
+             marks[1]=97;//chemistry
+             marks[2]=99;//biology
+             System.out.println(marks[0]);
+             System.out.println(marks[1]);
+             System.out.println(marks[2]);
+
+    }
+
+}
